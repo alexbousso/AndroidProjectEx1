c7a08525aafdec259f3a986772bf3b794d9bc3fe
Alex Bousso <alexbousso@gmail.com>
2016-03-17 21:14:14 +0200

    Initial commit

---
+++ app/src/main/java/com/alexbousso/ex1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.alexbousso.ex1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+}
