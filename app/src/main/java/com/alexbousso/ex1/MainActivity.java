bca05da938fc602a660080d981fb5bf902e5039e
Alex Bousso <alexbousso@gmail.com>
2016-03-18 17:26:22 +0200

    Finished implementing the exercise (assuming there are no bugs or RFC's...).

--- app/src/main/java/com/alexbousso/ex1/MainActivity.java
+++ app/src/main/java/com/alexbousso/ex1/MainActivity.java
@@ -5,25 +5,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    EditText editText;
-    Button makeOrderButton;
-    SeekBar seekBar;
+    private final int seekBarMaxValue = 100;
+
+    private EditText editText;
+    private Button makeOrderButton;
+    private SeekBar seekBar;
+    private CheckBox checkBox;
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.editText);
-        makeOrderButton = (Button) findViewById(R.id.makeOrderButton);
-        makeOrderButton.setEnabled(false);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        initializeComponents();
+    }
 
+    private void initializeComponents() {
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        editText = (EditText) findViewById(R.id.editText);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        makeOrderButton = (Button) findViewById(R.id.makeOrderButton);
+
+        initializeEditText();
+        initializeSeekBar();
+        initializeCheckBox();
+        initializeMakeOrderButton();
+    }
+
+    private void initializeMakeOrderButton() {
+        makeOrderButton.setEnabled(false);
+
+        makeOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = getResources().getString(R.string.OrderSent);
+                showToast(text);
+            }
+        });
+    }
+
+    private void initializeEditText() {
         editText.addTextChangedListener(new TextWatcher() {
             boolean ignoreChange = false;
 
@@ -37,15 +68,22 @@ public class MainActivity extends Activity {
                 if (!ignoreChange) {
                     String str = s.toString();
                     str = str.replace(".", "");
+
+                    Integer i = 0;
+                    try {
+                        if (str.length() > 0) {
+                            i = Integer.parseInt(str);
+                        }
+                    } catch (NumberFormatException ignored) {
+                    }
+
                     ignoreChange = true;
                     editText.setText(str);
                     editText.setSelection(editText.getText().length());
+                    setSeekBarProgress(i);
                     ignoreChange = false;
-                    if (str.length() > 0 && Integer.parseInt(str) > 0) {
-                        makeOrderButton.setEnabled(true);
-                    } else {
-                        makeOrderButton.setEnabled(false);
-                    }
+
+                    setMakeOrderButtonEnabledStatus();
                 }
             }
 
@@ -54,11 +92,17 @@ public class MainActivity extends Activity {
 
             }
         });
+    }
+
+    private void initializeSeekBar() {
+        seekBar.setMax(seekBarMaxValue);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                editText.setText(String.format("%d", progress));
+                if (fromUser) {
+                    editText.setText(String.format("%d", progress));
+                }
             }
 
             @Override
@@ -71,5 +115,54 @@ public class MainActivity extends Activity {
 
             }
         });
+    }
+
+    private void initializeCheckBox() {
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setMakeOrderButtonEnabledStatus();
+            }
+        });
+    }
+
+    private void setSeekBarProgress(Integer progress) {
+        if (progress >= 0 && progress <= seekBarMaxValue) {
+            seekBar.setProgress(progress);
+        } else if (progress > seekBarMaxValue) {
+            seekBar.setProgress(seekBarMaxValue);
+        } else if (progress < 0) {
+            seekBar.setProgress(0);
+        }
+    }
+
+    private void setMakeOrderButtonEnabledStatus() {
+        String str = editText.getText().toString();
+        try {
+            if (str.length() <= 0 || Integer.parseInt(str) <= 0) {
+                makeOrderButton.setEnabled(false);
+                return;
+            }
+        } catch (NumberFormatException e) {
+            String text = getResources().getString(R.string.BigNumberError);
+            showToast(text);
+            makeOrderButton.setEnabled(false);
+            return;
+        }
+
+        if (!checkBox.isChecked()) {
+            makeOrderButton.setEnabled(false);
+            return;
+        }
+
+        makeOrderButton.setEnabled(true);
+    }
+
+    private void showToast(String text) {
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
